515e7c9ffd14305610cfc6ca14b882e73032a85a
Jesse Leite <jesseleite@gmail.com>
2021-05-24 09:06:12 -0400

    Sync webpack.mix.js.

--- webpack.mix.js
+++ webpack.mix.js
@@ -7,12 +7,13 @@ require('laravel-mix-purgecss');
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
+ | for your Laravel applications. By default, we are compiling the CSS
  | file for the application as well as bundling up all the JS files.
  |
  */
 
 mix.js('resources/js/site.js', 'public/js')
+
 mix.postCss('resources/css/tailwind.css', 'public/css', [
   require('postcss-import'),
   require('tailwindcss'),
@@ -27,3 +28,21 @@ if (mix.inProduction()) {
     whitelistPatternsChildren: [/^content$/],
   });
 }
+
+/*
+ |--------------------------------------------------------------------------
+ | Statamic Control Panel
+ |--------------------------------------------------------------------------
+ |
+ | Feel free to add your own JS or CSS to the Statamic Control Panel.
+ | https://statamic.dev/extending/control-panel#adding-css-and-js-assets
+ |
+ */
+
+// mix.js('resources/js/cp.js', 'public/vendor/app/js')
+//    .postCss('resources/css/cp.css', 'public/vendor/app/css', [
+//     require('postcss-import'),
+//     require('tailwindcss'),
+//     require('postcss-nested'),
+//     require('postcss-preset-env')({stage: 0})
+// ])
