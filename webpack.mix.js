ed0788a7b6e6314faa8efb47984010e149c9e330
Jack McDade <jack@jackmcdade.com>
2022-03-07 09:59:58 -0500

    Upgrade to TailwindCSS 3 & Alpine 3

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,4 @@
 const mix = require('laravel-mix');
-require('laravel-mix-purgecss');
-
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -15,18 +13,13 @@ require('laravel-mix-purgecss');
 mix.js('resources/js/site.js', 'public/js')
 
 mix.postCss('resources/css/tailwind.css', 'public/css', [
-  require('postcss-import'),
-  require('tailwindcss'),
-  require('postcss-nested'),
-  require('postcss-preset-env')({stage: 0})
+    require('postcss-import'),
+    require('tailwindcss/nesting'),
+    require('tailwindcss'),
 ])
 
 if (mix.inProduction()) {
-  mix.version();
-  mix.purgeCss({
-    enabled: true,
-    whitelistPatternsChildren: [/^content$/],
-  });
+   mix.version();
 }
 
 /*
